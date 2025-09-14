5c9aa8d0014107ab1f5a75a68f7d26e3779f4ff3
TEJASZIMAN <tejasziman4521@gmail.com>
2025-09-14 20:08:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-//https://freemediatools.com/
-//    <footer class="MuiBox-root css-1k098vn"><div class="MuiContainer-root MuiContainer-maxWidthXl css-eb8ya3"><div class="MuiBox-root css-u7tflp"><button class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineHover MuiLink-button css-ec49xi" style="background: transparent; border: none; color: white;">About Us &nbsp;&nbsp; |</button><button class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineHover MuiLink-button css-ec49xi" style="background: transparent; border: none; color: white; margin-left: 15px;">FAQ &nbsp;&nbsp; |</button><button class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineHover MuiLink-button css-ec49xi" style="background: transparent; border: none; color: white; margin-left: 15px;">Contact Us &nbsp;&nbsp;  |</button></div><div class="MuiBox-root css-14h1e6v"><p class="MuiTypography-root MuiTypography-body2 css-1pzl2zk">© 2025 School Education and Sports Department, Government of Maharashtra.</p></div></div></footer>
 
-// shopify robbersoclout ----> private supplier
-// When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
@@ -11,11 +7,6 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
-//corvette c4 1984
-//1962 Chevrolet Corvair Monza GT
-//1951 Buick Lesabre
-//mako shark
-//1959 Cadillac Cyclone XP-74 concept car
-//Lotus Elan
-//corvette zl1 manta ray
-//honda prelude 1986
+function myFunction() {
+            alert("Other projects comming soon ");
+        }
